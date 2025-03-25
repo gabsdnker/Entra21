26a9109cdf0571bd6590fc9638694c897c40497f
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-03-25 11:00:15 -0300

    Add files via upload

---
+++ Java/MyProject/src/SemEntrada/ExercicioWhile.java
@@ -0,0 +1,15 @@
+//Autor: Gabrielli Danker
+import java.util.Scanner;
+
+public class ExercicioWhile {
+
+	public static void main(String[] args) {
+		
+		int numero = 20;
+		
+		while (numero >=10) {
+			System.out.println(numero);
+			numero --;
+		}	
+	}
+}

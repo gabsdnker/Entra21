21113247f61b5a7ec23c7ec48b8dd0d2ba8201db
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-03-25 07:59:47 -0300

    Update Exercicio19.java

--- Java/MyProject/src/Exercício Estrutura de Seleção/Exercicio19.java
+++ Java/MyProject/src/Exercício Estrutura de Seleção/Exercicio19.java
@@ -1,3 +1,4 @@
+//Autor: Gabrielli Danker
 import java.util.Scanner;
 public class Exercicio19{
     public  static void main(String[] args){
@@ -15,14 +16,16 @@ public class Exercicio19{
         System.out.println("Digite o valor por hora: ");
         double valorHora = input.nextDouble();
         
-        
         if (horasTrabalhadas == 160){
             double salarioTotal = horasTrabalhadas * valorHora;
+            
             System.out.println("Salário Total: " + salarioTotal);
+            
         } else if (horasTrabalhadas > 160){
             double horasExtras = horasTrabalhadas - 160;
             double valorExtras = horasExtras * 1.5;
             double salarioTotal = (160 * valorHora) + (horasExtras * valorExtras);
+            
             System.out.println("Salário total: " + salarioTotal);
         } else {
             System.out.println("Horas trabalhadas não cumpridas!");

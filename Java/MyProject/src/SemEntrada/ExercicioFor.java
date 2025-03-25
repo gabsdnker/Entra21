32534cd16328ac537b53a73ce04d77bfdffde83c
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-03-25 11:17:40 -0300

    Add files via upload

---
+++ Java/MyProject/src/SemEntrada/ExercicioFor.java
@@ -0,0 +1,14 @@
+
+public class ExercicioFor {
+
+	public static void main(String[] args) {
+		
+		for(int numero = 20; numero >= 10; numero --) {
+			System.out.println(numero + "\n");
+			for(int numero2 = 10; numero2 <= 20; numero2 ++) {
+				System.out.println(numero2);
+			}
+		}
+	}
+
+}

c136f8516cb57e7f7318226fe1a3cf23ad4e2cb9
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-04-14 11:15:16 -0300

    Update Exercicio1.java

--- Java/MyProject/src/Exercícios Métodos/Exercicio1.java
+++ Java/MyProject/src/Exercícios Métodos/Exercicio1.java
@@ -12,32 +12,32 @@
 import java.util.Scanner;
 
 public class Exercicio1 {
-	public static void verao() {
+	public static void imprimirVerao() {
 			System.out.println("É verão!\nE o tempo está quente!");
 	}
-	public static void outono() {
+	public static void imprimirOutono() {
 			System.out.println("É outono!\nO tempo esta fresco!");
 	}
-	public static void inverno() {
+	public static void imprimirInverno() {
 			System.out.println("É inverno!\nEstá frio!");
 	}
-	public static void primavera() {
+	public static void imprimirPrimavera() {
 		System.out.println("É primavera!\nAs flores estão nascendo!");		
 	}
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Digite um número de 1 a 4:");
+		System.out.println("Digite a estação:\n1- Verão\n2- Outono\n3- Inverno\n4- Primavera");
 		int estacao = input.nextInt();
 		
 		if (estacao == 1) {
-			verao();
+			 imprimirVerao();
 		} else if (estacao == 2) {
-			outono();
+			imprimirOutono();
 		} else if(estacao == 3) {
-			inverno();
+			imprimirInverno();
 		} else if (estacao == 4) {
-			primavera();
+			 imprimirPrimavera();
 		} else {
 			System.out.println("Inválido");
 		}

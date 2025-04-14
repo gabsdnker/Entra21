d9fa0a4e4ced77aa1c32914cf25332b7d543aa0e
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-04-14 09:54:00 -0300

    Create ExercicioMetodos.java

---
+++ Java/MyProject/src/Exercícios Métodos/ExercicioMetodos.java
@@ -0,0 +1,13 @@
+//Aluna: Gabrielli Danker
+public class ExercicoMetodos {
+	
+	public static void imprimirNome() {
+		System.out.println("Gabrielli");
+	}
+
+	public static void main(String[] args) {
+		imprimirNome();
+
+	}
+
+}

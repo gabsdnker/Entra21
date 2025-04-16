54eee69a53a71cc7cafa91f3821cd42b04e6d311
Gabrielli Danker <gabrielli.danker@gmail.com>
2025-04-16 11:27:39 -0300

    Exercício 8

---
+++ Java/MyProject/src/Exercícios Métodos/Exercicio8.java
@@ -0,0 +1,27 @@
+/*Faça um programa em que o usuário entre com um número de 1 a 4
+Com o número 1 sendo verão, 2 sendo outono...
+Dependendo de o que o usuário informa, retorne:
+É verão
+E o tempo está quente.
+Ou
+É inverno
+E está frio.
+Faça um método para cada estação do ano*/
+
+public class Exercicio8{
+    public static String verao(int estacao){
+        return "É verão!";
+    }
+    public static String outono(int estacao){
+
+    }
+    public static String inverno(int estacao){
+
+    }
+    public static String primaveiras(int estacao){
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}

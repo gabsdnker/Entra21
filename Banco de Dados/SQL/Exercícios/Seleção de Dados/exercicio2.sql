c662f3845465ea512ae3e2b5ad769d876f7dd5ce
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-05-30 09:48:01 -0300

    Create exercicio2.sql

---
+++ Banco de Dados/SQL/Exercícios/Seleção de Dados/exercicio2.sql
@@ -0,0 +1,20 @@
+# a) Listar todos os dados do modelos disponíveis
+
+SELECT *
+FROM modelo;
+
+# b) Listar a descrição de todas as marcas disponiveis
+
+SELECT ds_marca
+FROM marca;
+
+# c) Listar qual(is) proprietário(s) não possuem e-mail, mas que possuem número de telefone cadastrado
+
+SELECT nm_proprietario
+FROM proprietario
+WHERE ds_email IS NULL 
+	AND nr_telefone IS NOT NULL;
+	
+# d) Mostre os veículos fabricados após o ano de 20215, ordenados pelo ano de fabricação de forma decrescente 
+
+SELECT 

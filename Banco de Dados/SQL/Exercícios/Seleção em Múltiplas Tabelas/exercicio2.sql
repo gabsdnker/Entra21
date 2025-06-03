4a8dab2b96fe01745fe9cfd79d1e1bee3e6a67e4
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-06-03 08:46:08 -0300

    Create exercicio2.sql

---
+++ Banco de Dados/SQL/Exercícios/Seleção em Múltiplas Tabelas/exercicio2.sql
@@ -0,0 +1,15 @@
+#Utilizando o diagrama dos veículos:
+
+# a) Listar a marca e o modelo de todos os carros
+
+# b) Listar qual(is) proprietário(s) residem nos estados de "SC" ou "PR"
+
+# c) Listar as informações do(s) veículo(s) que apresenta(m) ano de fabricação diferente do ano modelo
+
+# d) Listar o nome do(s) proprietário(s) que residem em um bairro em que o nome tenha a palavra "Novo" ou "Nova" e que a UF seja de "SC"
+
+# e) Listar todas as marcas (descrição) disponíveis com o respetivo modelo (descrição), ordenando em ordem crescente de marca, seguida de modelo
+
+# f) Listar os veículos (com placa, descrição da marca, descrição do modelo) que possuem como acessório "Direção hidráulica"
+
+# g) Listar o número da placa do veículo, ano de fabricação e modelo do(s) veículos fabricados a partir de 2015, com menos de 50 mil quilômetros rodados, com 4 portas ou mais e que não possui(am) observações

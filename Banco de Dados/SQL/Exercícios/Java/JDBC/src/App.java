40c353d216d728ff43373532202db2d0e794f70c
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-06-04 09:36:22 -0300

    Add files via upload

---
+++ Banco de Dados/SQL/Exercícios/Java/JDBC/src/App.java
@@ -0,0 +1,24 @@
+
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) {
+        DepartamentoDAO depDAO = new DepartamentoDAO();
+
+        try {
+        	//depDAO.inserirDepartamento(new Departamento("Desenvolvimento"));
+            //depDAO.inserirDepartamento(new Departamento("Recursos Humanos"));
+        	//depDAO.inserirDepartamento(new Departamento("Limpeza"));
+        	
+        	//depDAO.atualizarDepartamento(new Departamento(3, "Analista"));
+        	
+        	depDAO.deletarDepartamento(2);
+        	
+        	System.out.println(depDAO.listarDepartamentos());
+        	
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}

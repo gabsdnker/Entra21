75d07ec8716c0e24f39d10e20f92b96854d3d30c
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-06-04 10:11:18 -0300

    Update App.java

--- Banco de Dados/SQL/Exercícios/Java/JDBC/src/App.java
+++ Banco de Dados/SQL/Exercícios/Java/JDBC/src/App.java
@@ -4,17 +4,26 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) {
         DepartamentoDAO depDAO = new DepartamentoDAO();
+        FuncionarioDAO funDAO = new FuncionarioDAO();
 
         try {
         	//depDAO.inserirDepartamento(new Departamento("Desenvolvimento"));
-             //depDAO.inserirDepartamento(new Departamento("Recursos Humanos"));
+            //depDAO.inserirDepartamento(new Departamento("Recursos Humanos"));
         	//depDAO.inserirDepartamento(new Departamento("Limpeza"));
         	
         	//depDAO.atualizarDepartamento(new Departamento(3, "Analista"));
         	
         	//depDAO.deletarDepartamento(2);
         	
-        	System.out.println(depDAO.listarDepartamentos());
+        	//System.out.println(depDAO.listarDepartamentos());
+        	
+        	//funDAO.inserirFuncionario(new Funcionario("José", "jose@email.com", 4000, 1));
+        	//funDAO.inserirFuncionario(new Funcionario("Joana", "joana@email.com", 2000, 3));
+        	//funDAO.inserirFuncionario(new Funcionario("Alexandre", "alexandre@email.com", 5000, 1));
+        	
+        	
+        	
+        	System.out.println(funDAO.listarFuncionarios());
         	
         } catch (SQLException ex) {
             ex.printStackTrace();

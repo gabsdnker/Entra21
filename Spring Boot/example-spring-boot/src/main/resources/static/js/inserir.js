90e0820302c0e7ede1b1ea79248ab10dea27e176
Gabrielli Danker <gabrielli.danker@gmail.com>
2025-07-29 09:03:54 -0300

    29/07/2025

--- Spring Boot/example-spring-boot/src/main/resources/static/js/inserir.js
+++ Spring Boot/example-spring-boot/src/main/resources/static/js/inserir.js
@@ -1,1 +1,22 @@
+document.getElementById('insertForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Impede o envio padrão do formulário
+    // Coleta os dados do formulário
+    const produto = {
+        nmProduto: document.getElementById('nome').value,
+        preco: parseFloat(document.getElementById('preco').value),
+        id: parseInt(document.getElementById('id').value)
+    };
 
+    // Envia os dados para o servidor
+    fetch('http://localhost:8080/produtos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'},
+        body: JSON.stringify(produto)
+    });
+    // Exibe uma mensagem de sucesso
+    alert('Produto inserido com sucesso!');
+    // Limpa os campos do formulário
+    document.getElementById('insertForm').reset();
+
+});
---
+++ Spring Boot/onecode.sql
@@ -0,0 +1,6 @@
+CREATE TABLE produto(
+id INTEGER,
+nm_produto VARCHAR(50),
+preco DECIMAL(10,2),
+PRIMARY KEY(id)
+);
